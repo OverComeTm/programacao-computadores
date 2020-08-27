b714fe20cea406ddb9e25cfab0af9b1b0ccaa0c0
OverComeTm <alexsousa3701@gmail.com>
2020-08-26 21:40:47 -0300

    exe7 calculando a média

---
+++ lista1-exe7.js
@@ -0,0 +1,27 @@
+chefe()
+
+function chefe(){
+    var vetor = [] // declara o vetor
+    exe7(vetor) // estamos passando o vetor como parâmetro - escopo global
+    calculaMedia(vetor)
+}
+
+function exe7(vet){ // vet representa vetor -> o que fizermos com vet, será feito em vetor
+
+    var numero = prompt(`Informe um numero`)
+    do {
+
+        // coloca o numero em vet
+        vet.push(numero)
+        numero = Number(prompt(`Informe outro numero, informe número negativo para encerrar`))
+    }
+    while (numero >= 0)
+
+}
+
+function calculaMedia(vet){
+    var soma = 0 
+    for(var i=0; i < vet.lenght;i++){
+        soma = soma + vet[i]
+    }
+}
