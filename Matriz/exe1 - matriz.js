07c500977d71633c1a1e23f73a468f9474b1eba8
OverComeTm <alexsousa3701@gmail.com>
2020-09-15 20:39:26 -0300

    exe1 matriz atualizado 15/09

--- Matriz/exe1 - matriz.js
+++ Matriz/exe1 - matriz.js
@@ -3,33 +3,64 @@ chefe()
 function chefe(){
     //declara a matriz
     let matriz = [] // uma matriz é um vetor 
-    //vamos criar a matriz com 3 linhas
-    matriz[0] = []
-    matriz[1] = []
-    matriz[2] = []
-    cadastra(matriz) // cadastra 3 alunos
-    consultaMediaAlunos(matriz) // calcula a média das notas
+    //vamos criar o vetor de alunos
+    let vetor = []
+    cadastra(matriz, vetor) // cadastra 3 alunos
+    consultaMediaAlunos(matriz, vetor) // calcula a média das notas
+    calculaMediaProvas(matriz)
+    calculaMaiorNotaProva(matriz, vetor)
 }
 
-function cadastra(mat){ // mat representa a matriz na função chefe()
-    for(let i=0; i<3; i++){
-        alert(`Informe 4 notas do aluno ${i+1}`)
-        for(let j=0; j<4; j++){
+function calculaMaiorNotaProva(mat, vet){
+    for(let j=0;j<4;j++){ // para cada prova
+        let maiorNota = mat[0][j] // assumo que a maior nota é do primeiro 
+        let nomeMaiorNota = vet[j].nome // assumo que o nome do aluno com maior nota é o 1.o bimestre
+        for(let i=0;i<3;i++){
+            if (mat[i][j] > maiorNota){
+                maiorNota = mat[i][j] //atualiza nota 
+                nomeMaiorNota = vet[j].nome //atualiza nome
+            }
+        }
+        console.log(`A prova ${j+1} tem a maior nota ${maiorNota} do aluno ${nomeMaiorNota}`)
+    }
+}
+
+function calculaMediaProvas(mat){
+    for(let j=0;j<4;j++){
+        let media = 0
+        for(let i=0;i<3;i++){
+            media = media + mat[i][j]
+        }
+        console.log(`A média da prova ${j+1} é ${media/3}`)
+    }
+}
+// mat representa a matriz na função chefe()
+// vet representa a matriz na função chefe()
+function cadastra(mat, vet){ // mat representa a matriz na função chefe()
+    for(let i=0; i<3; i++){ // sao 3 alunos
+        var objeto = new Object ()
+        objeto.nome = prompt(`Informe o nome`)
+        objeto.rg = prompt(`Informe o RG`)
+        objeto.media = 0 // vamos considerar que no cadastro a média do aluno é 0
+        // insere o objeto em vet
+        vet.push(objeto)
+        mat[i] = [] // cada linha da matriz é um vetor
+        alert(`Informe 4 notas do aluno ${objeto.nome}`)
+        for(let j=0; j<4; j++){ // sao 4 notas
             mat[i][j] = Number(prompt(`Informe a nota ${j+1}`))
         }
     }
 }
 
-function consultaMediaAlunos(mat){// mat representa a matriz na função chefe()
-    let media = [0,0,0]
+function consultaMediaAlunos(mat, vet){// mat representa a matriz na função chefe()
     for(let i=0; i<3; i++){ // para cada alunos
         for(let j=0; j<4; j++){ // para cada nota
             // soma no vetor na posição i
-            medias[i] = medias[i] + mat[i][j]
+            vet[i].media = vet[i].media + mat[i][j]
         }
         // acabaram as notas do alunos i
-        medias[i] = medias[i] / 4
+        vet[i].media = vet[i].media / 4
     }
     // acabaram os alunos
-    console.log(`Valor das Médias ${medias}`)
+    console.log(vet)
 }
